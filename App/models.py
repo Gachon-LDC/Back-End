e60e830cfd5063be3455a0359d30b38ee0142c47
Insiro <leeon@insiro.me>
2022-11-28 19:00:15 +0900

    add: video model base structure

--- App/models.py
+++ App/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    UUIDField,
+    TextField,
+    JSONField,
+    IntegerField,
+    FileField,
+)
 
 # Create your models here.
+class VideoModel(Model):
+    id = UUIDField()
+    uploader_id = TextField()
+    post_id = TextField()
+    angles = JSONField()
+    # meta data
+    path = FileField()
+    video_lenghth = IntegerField()
+    fps = IntegerField()
