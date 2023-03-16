323b535048f7eaa40bb6481d330b984cd1e3b95b
insiro <leeon@insiro.me>
2023-03-17 02:03:42 +0900

    refactor: VideoModel, VideoSerializer add: title filed on VideoModel remove: file_path on Video Model add: serialize rules for videomodle

--- App/serializers.py
+++ App/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from App.models import VideoModel
-import os
 
 
 class VideoModelSerializer(serializers.ModelSerializer):
+    video_id = serializers.UUIDField(required=False)
+    uploader_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    dance = serializers.PrimaryKeyRelatedField(read_only=True)
+    fps = serializers.IntegerField(required=False)
+
     class Meta:
         model = VideoModel
         fields = ["__all__"]
+        exclude = ["embeds"]
 
     pass
